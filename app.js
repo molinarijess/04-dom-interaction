b8c31c9f46b04258e9cd2b95b8a6acff95e3bf26
molinarijess <lacerdajess@gmail.com>
2022-11-15 16:16:31 +0000

    Update app.js

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ const deleteMovieHandler = (movieId) => {
   movies.splice(movieIndex, 1);
   listRoot.children[movieIndex].remove();
   closeMovieDeletionModal();
+  updateUI();
 };
 
 const startDeleteMovieHandler = (movieId) => {
